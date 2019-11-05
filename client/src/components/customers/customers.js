64704465a7d14cdc343b347e77ddc1aa7bc30de5
pitmal <pitmalik@interia.pl>
2019-11-05 15:26:50 +0100

    fetched customers from express

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "./customers.css";
 class Customers extends Component {
+  constructor() {
+    super();
+    this.state = { customers: [] };
+  }
+  componentDidMount() {
+    fetch("/api/customers", { method: "GET" })
+      .then(res => res.json())
+      .then(customers =>
+        this.setState({ customers }, () =>
+          console.log("Fetched customers", customers)
+        )
+      );
+  }
   render() {
-    return (
-      <>
-        <h1>Customers</h1>
-      </>
-    );
+    const customers = this.state.customers.map(customer => (
+      <li key={customer.id}>
+        {customer.firstName} {customer.lastName}
+      </li>
+    ));
+    return <ul>{customers}</ul>;
   }
 }
 export default Customers;
