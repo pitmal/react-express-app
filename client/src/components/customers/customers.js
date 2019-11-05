2d6b2f52c17604c5cc2dda5d7bfddfe6d52c9afc
pitmal <pitmalik@interia.pl>
2019-11-05 15:05:02 +0100

    define cutomers component

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./customers.css";
+class Customers extends Component {
+  render() {
+    return (
+      <>
+        <h1>Customers</h1>
+      </>
+    );
+  }
+}
+export default Customers;
